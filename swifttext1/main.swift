336b9d3c9317d1995acd67d8557fe0c7fd1140e9
20131105808 <a20131105808@mac23.local>
2016-03-03 10:29:25 +0800

    Initial Commit

---
+++ swifttext1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swifttext1
+//
+//  Created by 20131105808 on 16/3/3.
+//  Copyright © 2016年 20131105808. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
